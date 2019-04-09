b0c711a31a96d1bfa6e14c4b3afb53dc849f1b29
Aiden Gleave <aidengleave@gmail.com>
2019-04-09 21:45:58 +0100

    feat: added click for X function

--- src/index.js
+++ src/index.js
@@ -4,11 +4,17 @@ import './index.css';
 import { className } from 'postcss-selector-parser';
 
 class Square extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      value: null,
+    };
+  }
   render() {
     return (
-      <button className="square" onClick={() => 
-        alert('click')}>
-        {this.props.value}
+      <button className="square" 
+        onClick={() => this.setState({value: 'X'})}>
+        {this.state.value}
       </button>
     );
   }
