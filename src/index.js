03fa24f29fca5820777e972c038366aca13a400e
Aiden Gleave <aidengleave@gmail.com>
2019-04-10 11:34:34 +0100

    fix: square numbering typo

--- src/index.js
+++ src/index.js
@@ -57,14 +57,14 @@ class Board extends React.Component {
           {this.renderSquare(2)}
         </div>
         <div className="board-row">
+          {this.renderSquare(3)}
           {this.renderSquare(4)}
-          {this.renderSquare(5)}
-          {this.renderSquare(6)}                  
+          {this.renderSquare(5)}                  
         </div>
         <div className="board-row">
+          {this.renderSquare(6)}                  
           {this.renderSquare(7)}                  
           {this.renderSquare(8)}                  
-          {this.renderSquare(9)}                  
         </div>
       </div>
     );
